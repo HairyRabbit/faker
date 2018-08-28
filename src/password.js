9837675d550a36ced666dcf35b88c38f41f7ee84
Rabbit <yfhj1990@hotmail.com>
2018-08-28 11:26:01 +0800

    Fix password min length bug

--- src/password.js
+++ src/password.js
@@ -4,16 +4,25 @@
  * @flow
  */
 
-import { repeat, pick, minmax, createFaker } from './'
+import { repeat, oneof, minmax, createFaker } from './'
 import { db as charDb } from './char'
 import { db as symbolDb } from './symbol'
 
-const fake = createFaker('password', {
+export const passwordMinLength = 6
+export const passwordMaxLength = 20
+
+export const fake = createFaker('password', {
   default: {
     db: [].concat(charDb, symbolDb),
     proc(db, { min, max }) {
-      const len = minmax(6, 20, min, max)
-      return pick(len, db).join('')
+      const len = minmax(
+        passwordMinLength,
+        passwordMaxLength,
+        min,
+        max
+      )
+
+      return repeat(len, () => oneof(db)).join('')
     }
   }
 })
@@ -40,6 +49,8 @@ describe('random password', function() {
     repeat(1e3, () => {
       const gen = fake({ min: 10, max: 30 })
       assert(gen.length >= 10 && gen.length <= 30)
+      const gen2 = fake({ min: 128 })
+      assert(gen2.length === 128)
     })
   })
 })
